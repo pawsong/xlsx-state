e70c11f9625cb29e6bf53e023f2a288d9160abe7
Seunggeun Song <seunggeun@nector.co>
2017-09-28 20:10:49 +0900

    Move exports to bottom of file

--- src/index.ts
+++ src/index.ts
@@ -457,28 +457,6 @@ class RefValue {
   }
 }
 
-export function col_str_2_int(col_str) {
-  let r = 0;
-  const colstr = col_str.replace(/[0-9]+$/, '');
-  for (let i = colstr.length; i--;) {
-    r += Math.pow(26, colstr.length - i - 1) * (colstr.charCodeAt(i) - 64);
-  }
-  return r - 1;
-}
-
-export function int_2_col_str(n) {
-  let dividend = n + 1;
-  let columnName = '';
-  let modulo;
-  let guard = 10;
-  while (dividend > 0 && guard --) {
-      modulo = (dividend - 1) % 26;
-      columnName = String.fromCharCode(modulo + 65) + columnName;
-      dividend = (dividend - modulo - 1) / 26;
-  }
-  return columnName;
-}
-
 class Range {
   constructor(private str_expression, private formula) {}
 
@@ -784,6 +762,28 @@ function find_all_cells_with_formulas(wb) {
     }
   }
   return cells;
+}
+
+export function col_str_2_int(col_str) {
+  let r = 0;
+  const colstr = col_str.replace(/[0-9]+$/, '');
+  for (let i = colstr.length; i--;) {
+    r += Math.pow(26, colstr.length - i - 1) * (colstr.charCodeAt(i) - 64);
+  }
+  return r - 1;
+}
+
+export function int_2_col_str(n) {
+  let dividend = n + 1;
+  let columnName = '';
+  let modulo;
+  let guard = 10;
+  while (dividend > 0 && guard --) {
+      modulo = (dividend - 1) % 26;
+      columnName = String.fromCharCode(modulo + 65) + columnName;
+      dividend = (dividend - modulo - 1) / 26;
+  }
+  return columnName;
 }
 
 export function set_fx(name, fn) {
